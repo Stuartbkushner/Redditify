3bf133da79a92d5a4eceb5da3be8448c858d3901
Stuart Kushner <stuartbkushner@gmail.com>
2020-04-27 15:08:01 -0400

    bootstrap added again

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import PlaylistView from './components/PlaylistView'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={PlaylistView} />
+        </div>
+      </Router>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/PlaylistView.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class PlaylistView extends Component {
+  render() {
+    return (
+      <div>
+        <div className='container'>
+          <h1 className='title'>Redditify</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default PlaylistView
