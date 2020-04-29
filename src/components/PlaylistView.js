173c236b8874fbbaa19791edce02a1260b7dd77d
Stuart Kushner <stuartbkushner@gmail.com>
2020-04-29 13:33:16 -0400

    Data now passes to the screen

--- src/components/PlaylistView.js
+++ src/components/PlaylistView.js
@@ -1,12 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import RedditAPI from '../models/RedditAPI'
 
 class PlaylistView extends Component {
+  state = { posts: null }
+
+  constructor(props) {
+    super(props)
+    this.redditAPI = new RedditAPI()
+  }
+
+  async componentDidMount() {
+    const posts = await this.redditAPI.spotifyPosts()
+    this.setState(prevState => ({ posts }))
+  }
+
   render() {
+    const { posts } = this.state
+
     return (
       <div>
-        <div className='container'>
-          <h1 className='title'>Redditify</h1>
-        </div>
+        <section className="hero is-link">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">Redditify</h1>
+            </div>
+          </div>
+        </section>
+        <section className="section">
+          <div className="container">
+            {posts ? (
+              <ul>
+                {posts.map(post => {
+                  return (
+                    <li key={post.id}>
+                      {post.title}
+                    </li>
+                  )
+                })}
+              </ul>
+            ) : (
+              <p>Loading...</p>
+            )}
+          </div>
+        </section>
       </div>
     )
   }
